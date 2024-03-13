5485b8216ed2635893cd17fff5fbb426c20f73bc
Asanka-GAP <asankapradeep0830@gmail.com>
2024-03-13 21:23:15 +0530

    Updated

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-function changeView(){
-    var signUpBox = document.getElementById("signUpBox");
-    var signInBox = document.getElementById("signInBox");
+function changeView() {
+  var signUpBox = document.getElementById("signUpBox");
+  var signInBox = document.getElementById("signInBox");
 
-    signUpBox.classList.toggle("d-none");
-    signInBox.classList.toggle("d-none");
+  signUpBox.classList.toggle("d-none");
+  signInBox.classList.toggle("d-none");
 }
 
-function signup(){
-    var fname=document.getElementById("fname");
-    var lname=document.getElementById("lname");
-    var email=document.getElementById("email");
-    var password=document.getElementById("password");
-    var mobile=document.getElementById("mobile");
-    var gender=document.getElementById("gender");
+function signup() {
+  var fname = document.getElementById("fname");
+  var lname = document.getElementById("lname");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  var mobile = document.getElementById("mobile");
+  var gender = document.getElementById("gender");
 
-    alert(fname.value);
-    alert(lname.value);
+  alert(fname.value);
+  alert(lname.value);
 
-    var form = new FormData();
+  var form = new FormData();
 
-    form.append("f",fname.value);
-    form.append("l",lname.value);
-    form.append("e",email.value);
-    form.append("p",password.value);
-    form.append("m",mobile.value);
-    form.append("g",gender.value);
-    
-    
-    
+  form.append("f", fname.value);
+  form.append("l", lname.value);
+  form.append("e", email.value);
+  form.append("p", password.value);
+  form.append("m", mobile.value);
+  form.append("g", gender.value);
 
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function () {
+    if (request.status == 200 && request.readyState == 4) {
+      var response = request.responseText;
+      alert(response);
+    }
+  };
+
+  request.open("POST", "signUpProcess.php", true);
+  request.send(form);
 }
