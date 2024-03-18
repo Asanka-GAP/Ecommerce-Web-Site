062cae6a2e1863de01a9166f1ac54b6fcd9606ef
Asanka-GAP <asankapradeep0830@gmail.com>
2024-03-18 21:10:41 +0530

    Updated

--- script.js
+++ script.js
@@ -43,3 +43,29 @@ function signup() {
   request.open("POST", "signUpProcess.php", true);
   request.send(form);
 }
+
+function signin() {
+
+  var email = document.getElementById("email2");
+  var password = document.getElementById("password2");
+  var rememberme = document.getElementById("rememberme");
+
+  
+  var form = new FormData();
+  form.append("e",email.value);
+  form.append("p",password.value);
+  form.append("r",rememberme.checked);
+
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function(){
+    if(request.status == 200 && request.readyState==4){
+      var response = request.responseText;
+      alert(response);
+    }
+  }
+
+  request.open("POST","signinProcess.php",true);
+  request.send(form);
+
+}
