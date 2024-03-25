76a953f1389b6ce422e1bef432e348de98418118
Asanka-GAP <asankapradeep0830@gmail.com>
2024-03-25 21:42:01 +0530

    updated

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style.css" />
 
-    <link rel="icon" href="resource/logo.svg" />
+    <link rel="icon" href="resources/logo.svg" />
 
 </head>
 
